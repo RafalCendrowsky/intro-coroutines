ba49d28be10da52c7d95b349591f4e1067fd5423
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2023-03-10 15:01:39 +0100

    create fork for old local changes

--- src/tasks/Request5NotCancellable.kt
+++ src/tasks/Request5NotCancellable.kt
@@ -5,5 +5,21 @@ import kotlinx.coroutines.*
 import kotlin.coroutines.coroutineContext
 
 suspend fun loadContributorsNotCancellable(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: listOf()
+
+    val contributors : List<Deferred<List<User>>> = repos.map { repo ->
+        GlobalScope.async {
+            delay(3000)
+            service
+                .getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+        }
+    }
+
+    return contributors.awaitAll().flatten().aggregate()
+
 }
--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -1,7 +1,9 @@
 package tasks
 
 import contributors.*
+import kotlinx.coroutines.async
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
@@ -11,6 +13,22 @@ suspend fun loadContributorsChannels(
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
     coroutineScope {
-        TODO()
+        val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .body() ?: listOf()
+        var allUsers = emptyList<User>()
+        val userChannel = Channel<List<User>>(UNLIMITED)
+        repos.forEach { repo ->
+            launch {
+                service.getRepoContributors(req.org, repo.name).body()?.let { userChannel.send(it) }
+            }
+        }
+        repeat(repos.size) {
+            val users = userChannel.receive()
+            allUsers = (allUsers + users).aggregate()
+            updateResults(allUsers, false)
+        }
+        updateResults(allUsers, true)
     }
 }
